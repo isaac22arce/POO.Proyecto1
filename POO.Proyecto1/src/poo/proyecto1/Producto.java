f42b9551139c2893a6c1f146d6e2d2b78037437a
isaac22arce <67889523+isaac22arce@users.noreply.github.com>
2020-11-20 19:08:55 -0500

    primer sumit

--- POO.Proyecto1/src/poo/proyecto1/Producto.java
+++ POO.Proyecto1/src/poo/proyecto1/Producto.java
@@ -9,13 +9,6 @@ package poo.proyecto1;
  *
  * @author Isaac
  */
-public class POOProyecto1 {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
+public class Producto {
     
 }
