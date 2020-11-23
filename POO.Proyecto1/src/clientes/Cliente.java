511c102ab9d4be4180baa7669c77b2046830c211
isaac22arce <67889523+isaac22arce@users.noreply.github.com>
2020-11-23 14:01:57 -0500

    cliente

---
+++ POO.Proyecto1/src/clientes/Cliente.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package clientes;
+
+/**
+ *
+ * @author Isaac
+ */
+public class Cliente {
+    protected String cedula;
+    protected String nombres;
+    protected String apellidos;
+    protected String telefono;
+    protected String correo;
+    protected String direccion;
+    
+    //metodos
+    public Cliente(String cedula, String nombres ,String apellidos, String telefono, String correo, String direccion){
+       this.cedula=cedula;
+       this.nombres= nombres;
+       this.apellidos= apellidos;
+       this.telefono=telefono;
+       this.correo=correo;
+       this.direccion=direccion;
+    }
+    
+}
---
+++ POO.Proyecto1/src/clientes/Vip.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package clientes;
+
+/**
+ *
+ * @author Isaac
+ */
+public class Vip extends Cliente{
+    private int peso;
+    private int estatura;
+    private int hes;
+    private String profesion;
+    
+    //metodos
+    public Vip(String cedula, String nombres, String apellidos, String telefono, String correo,String direccion,int peso,int estatura,int hes,String profesion ){
+        super(cedula,nombres,apellidos,telefono,correo, direccion);
+        this.peso=peso;
+        this.estatura=estatura;
+        this.hes=hes;
+        this.profesion=profesion;
+    }
+}
