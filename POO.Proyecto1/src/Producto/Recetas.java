1b8cfb380d0472aee19d0c3f10cdbbb60438e44f
AlexandraImaicela <74851379+AlexandraImaicela@users.noreply.github.com>
2020-11-24 22:26:22 -0500

    Add files via upload

---
+++ POO.Proyecto1/src/Producto/Recetas.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Producto;
+
+/**
+ *
+ * @author lenovo
+ */
+public class Recetas extends Producto{
+    
+    public Recetas(String nombreProducto) {
+        super(nombreProducto);
+    }
+    
+}
