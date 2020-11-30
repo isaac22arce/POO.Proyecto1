ee70481099c8d5054e3020a323aa7e523a17f78b
Alexandra Imaicela <aeimaice@espol.edu.ec>
2020-11-29 20:38:11 -0500

    Creacion de constructor en la clase Producto

--- POO.Proyecto1/src/Recetario/Producto.java
+++ POO.Proyecto1/src/Recetario/Producto.java
@@ -7,8 +7,19 @@ package Recetario;
 
 /**
  *
- * @author lenovo
+ * @author Alexandra Imaicela
  */
 public class Producto {
+    private String nombre;
+    private InformacionNutricional InfoNutricionalx100g;
+
+    public Producto(String nombre, InformacionNutricional InfoNutricionalx100g) {
+        this.nombre = nombre;
+        this.InfoNutricionalx100g = InfoNutricionalx100g;
+    }
+    
+    public Producto(String nombre) {
+        this.nombre = nombre;
+    }
     
 }
