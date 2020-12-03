f684259e397c3af4b05fc64ae71f58b8e0cdd351
Alexandra Imaicela <aeimaice@espol.edu.ec>
2020-12-03 12:24:57 -0500

    Creación del método enviarCorreo

--- POO.Proyecto1/src/Mail/Correo.java
+++ POO.Proyecto1/src/Mail/Correo.java
@@ -4,7 +4,17 @@
  * and open the template in the editor.
  */
 package Mail;
-
+import javax.mail.*;
+import java.util.Properties;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
 /**
  *
  * @author Alexandra Imaicela
@@ -12,5 +22,36 @@ package Mail;
 public class Correo {
      private final static String REMITENTE = "nutrifit456@gmail.com";
      private final static String CONTRASENA= "Contra,1";
-    
+     
+    public Correo(){
+    }
+    public void enviarCorreo(String correoReceptor, String asunto, String mensaje) {
+        Properties prop = new Properties();
+        prop.put("mail.smtp.host", "smtp.gmail.com");
+        prop.put("mail.smtp.port", "587");
+        prop.put("mail.smtp.auth", "true");
+        prop.put("mail.smtp.starttls.enable", "true"); //TLS
+        
+        Session session = Session.getInstance(prop,
+                new javax.mail.Authenticator() {
+                    protected PasswordAuthentication getPasswordAuthentication() {
+                        return new PasswordAuthentication(REMITENTE, CONTRASENA);
+                    }
+                });
+
+        try {
+            Message message = new MimeMessage(session);
+            message.setFrom(new InternetAddress(REMITENTE));
+            message.setRecipients(
+                    Message.RecipientType.TO,
+                    InternetAddress.parse(correoReceptor)
+            );
+            message.setSubject("Testing Gmail TLS");
+            message.setText(mensaje);
+            Transport.send(message);
+            System.out.println("Done");
+        } catch (MessagingException e) {
+            e.printStackTrace();
+        }
+    }   
 }
